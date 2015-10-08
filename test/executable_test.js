d4acf4aa8769197c40141451473b0eb7de1331aa
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-10-09 04:02:37 +0800

    Add nixt test

---
+++ test/executable_test.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const nixt = require('nixt')
+const fixture = require('./support/fixture')
+const join = require('path').join
+
+describe('app', function () {
+  this.timeout(5000)
+
+  var fx = fixture('basic')
+
+  beforeEach(function () {
+    process.cwd(fx.path())
+    this.nixt = nixt()
+      .run('rm -rf _docpress')
+      .cwd(join(__dirname, '../bin'))
+  })
+
+  it('works', function (next) {
+    this.nixt.run('docpress')
+    .end(() => {
+      expect(fx.exists('_docpress/index.html')).toEqual(true)
+      next()
+    })
+  })
+})
